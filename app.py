f86137ebe049ae877de39ba2670b855c39472147
KC21025 <21025@rosmini.school.nz>
2026-03-11 23:50:21 +0000

    I added the stats.html, and routed it using flask. I've added the stats in and it is working.

--- app.py
+++ app.py
@@ -23,5 +23,14 @@ def manage():
     conn.close() # Closing the database connection
     return render_template('manage.html', books=books)  # Render the manage.html template and pass books variable to it
 
+@app.route('/stats') # Define a route for stats url
+def stats():
+    conn = get_db_connection() # Getting database connection
+    total_books = conn.execute("SELECT COUNT(*) From ITEM").fetchall()[0]
+    books_to_read = conn.execute("SELECT Count(Item.Status_ID) FROM ITEM where Status_ID = 3").fetchall()[0]
+    books_reading = conn.execute("SELECT Count(Item.Status_ID) FROM ITEM where Status_ID = 1").fetchall()[0]
+    conn.close() # Closing the database connection
+    return render_template('stats.html', total_books=total_books, books_to_read=books_to_read, books_reading=books_reading)
+
 if __name__ == '__main__':
     app.run(debug=True) # Run the Flask application in debug mode
