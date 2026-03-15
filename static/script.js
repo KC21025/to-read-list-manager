6c10161af3bf36dceb494ede90a22be54cb5ca77
KC21025 <21025@rosmini.school.nz>
2026-03-15 23:26:36 +0000

    Added javascript to make a pop up for the book adding form

---
+++ static/script.js
@@ -0,0 +1,17 @@
+const modal = document.getElementById('bookModal');
+const button = document .getElementById('openModal');
+const span = document.getElementsByClassName('close')[0];
+
+button.onclick = function() {
+    modal.style.display = 'block';
+}
+
+span.onclick = function() {
+    modal.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
